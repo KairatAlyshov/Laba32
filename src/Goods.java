0813fbcdb709baed0eaf3dd3ea51917d64f09b02
Tolobekova Aiganysh <tolobekova01@gmail.com>
2023-01-09 17:00:18 +0600

    add some actions

---
+++ src/Goods.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.Random;
+
+public class Goods {
+    private String name;
+    private int price;
+    private static final List<String> names = List.of("BananaChips", "Candy", "Water", "Coca-cola");
+
+    public Goods(int i) {
+        Random rnd = new Random();
+        name = names.get(i);
+        price = rnd.nextInt(36)+5;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
